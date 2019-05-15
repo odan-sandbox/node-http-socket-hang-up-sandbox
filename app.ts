7fd35a0cc66517639cfd32553a20b14a7b10511f
odan <odan3240@gmail.com>
2019-05-16 00:59:41 +0900

    reproduction

---
+++ app.ts
@@ -0,0 +1,54 @@
+import http from "http";
+import { exec } from "child_process";
+
+const sleep = (msec: number) =>
+  new Promise(resolve => setTimeout(resolve, msec));
+
+async function init() {
+    const command = "docker run --rm -p 5650:80 nginx";
+    const appProcess = exec(command);
+    appProcess.stdout!.pipe(process.stdout);
+    appProcess.stderr!.pipe(process.stderr);
+    process.on("exit", () => {
+      appProcess.kill();
+    });
+  
+    console.log("sleep...");
+    await sleep(5000);
+}
+
+async function request (url: string) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(url, res => {
+      res.on("data", chunk => {
+        console.log(`BODY: ${chunk.length}`);
+      });
+      res.on("end", () => {
+        console.log("No more data in response.");
+      });
+      resolve()
+    });
+    req.on("error", e => {
+      console.log(`problem with request: ${e.message}`);
+      reject(e)
+    });
+    req.end();
+
+  })
+}
+
+async function main() {
+  await init()
+
+  console.log("request");
+  const url = "http://localhost:5650";
+
+  const count = 10000;
+  for (let i = 0; i < count; i++) {
+    console.log(i)
+    await request(url)
+  }
+
+}
+
+main();
